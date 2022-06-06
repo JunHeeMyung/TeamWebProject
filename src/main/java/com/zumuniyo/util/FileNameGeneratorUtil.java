67580bfc3b111cef7ec19a5d2307a28ee4946391
JunHee Myung <platinyan@naver.com>
2022-06-07 07:13:19 +0900

    파일이름생성기

---
+++ src/main/java/com/zumuniyo/util/FileNameGeneratorUtil.java
@@ -0,0 +1,20 @@
+package com.zumuniyo.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class FileNameGeneratorUtil {
+
+	private FileNameGeneratorUtil() {}
+	
+	static String generate(String fileName) {
+		
+		String extension =  fileName.indexOf(".")>=0?fileName.substring(fileName.lastIndexOf(".")):"";
+		String date = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()); 
+		StringBuilder random = new StringBuilder("");
+		for(int i=0;i<100;i++) {
+			random.append(((int)(Math.random()*10)));
+		}
+		return date+random+extension;
+	}	
+}
