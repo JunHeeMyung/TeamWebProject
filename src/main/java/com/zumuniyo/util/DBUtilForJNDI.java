356a86921f6e671334669fafdd751565e1dadb59
JunHee Myung <platinyan@naver.com>
2022-06-08 01:08:54 +0900

    JDNI-DBUtil

---
+++ src/main/java/com/zumuniyo/util/DBUtilForJNDI.java
@@ -0,0 +1,40 @@
+package com.zumuniyo.util;
+
+import java.sql.*;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DBUtilForJNDI {
+
+	public static Connection getConnection() {
+
+		Context initContext = null;
+		Connection conn = null;
+
+		try {
+			initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			DataSource ds = (DataSource) envContext.lookup("jdbc/myoracle");
+			conn = ds.getConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return conn;
+	}
+
+	public static void dbClose(ResultSet rs, Statement st, Connection conn) {
+
+		try {
+			if (rs != null) rs.close();
+			if (st != null) st.close();
+			if (conn != null) conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
