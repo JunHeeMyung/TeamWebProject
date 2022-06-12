f0c963cfcd0e233824b76f1319b0e8e57a3aad72
JunHee Myung <platinyan@naver.com>
2022-06-12 22:32:46 +0900

    페이지필터

--- src/main/java/com/zumuniyo/filter/PageFilter.java
+++ src/main/java/com/zumuniyo/filter/PageFilter.java
@@ -14,23 +14,24 @@ import javax.servlet.http.HttpSession;
 @WebFilter("*.do")
 public class PageFilter implements Filter {
 
+	
+	// 방식 변경필요
     
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		
-		// 로그인시 마지막 페이지로 이동하기위해 저장함
+		// 로그인/로그아웃시 마지막 페이지로 이동하기위해 저장함
 		
 		String uri = ((HttpServletRequest)request).getRequestURI();
 		String path = ((HttpServletRequest)request).getContextPath();
 		
 		HttpSession session = ((HttpServletRequest)request).getSession();
 		
-		System.out.println(path);
-		System.out.println(uri);
-		
 		if(!uri.equals(path+"/member/login.do")
 		 &&!uri.equals(path+"/member/logout.do")
-		 &&!uri.equals(path+"/member/register.do")
 		 &&!uri.equals(path+"/member/login/sublogin.do")
+		 &&!uri.equals(path+"/member/register.do")
+		 &&!uri.equals(path+"/member/idcheck.do")
+		 &&!uri.equals(path+"/member/nickcheck.do")
 		 &&!uri.equals(path+"/member/login/subregister.do")) {
 			session.setAttribute("nextpage", uri);
 		}
