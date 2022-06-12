f2d955d6679efe29e1fc2878a487ce5efa92694f
JunHee Myung <platinyan@naver.com>
2022-06-12 09:53:03 +0900

    모든경로 필터에 path 세션으로저장

--- src/main/java/com/zumuniyo/filter/EncodeFilter.java
+++ src/main/java/com/zumuniyo/filter/EncodeFilter.java
@@ -7,12 +7,19 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @WebFilter("/*")
 public class EncodeFilter implements Filter {
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		
+		// contextPath 설정
+		HttpSession session = ((HttpServletRequest)request).getSession();		
+		session.setAttribute("path",((HttpServletRequest)request).getContextPath()); 
+		
+		// 인코딩설정
 		request.setCharacterEncoding("utf-8");
 		chain.doFilter(request, response);
 		
