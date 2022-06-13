a04253d0a1a0d8d700b1f7bf527dc76ee5e6911d
JunHee Myung <platinyan@naver.com>
2022-06-13 09:49:46 +0900

    로그인/로그아웃시 돌아가는 페이지 확장명: .zmny

--- src/main/java/com/zumuniyo/filter/PageFilter.java
+++ src/main/java/com/zumuniyo/filter/PageFilter.java
@@ -11,9 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 
-@WebFilter("*.do")
+@WebFilter("*.zmny")
 public class PageFilter implements Filter {
-
 	
 	// 방식 변경필요
     
@@ -22,19 +21,12 @@ public class PageFilter implements Filter {
 		// 로그인/로그아웃시 마지막 페이지로 이동하기위해 저장함
 		
 		String uri = ((HttpServletRequest)request).getRequestURI();
-		String path = ((HttpServletRequest)request).getContextPath();
 		
 		HttpSession session = ((HttpServletRequest)request).getSession();
 		
-		if(!uri.equals(path+"/member/login.do")
-		 &&!uri.equals(path+"/member/logout.do")
-		 &&!uri.equals(path+"/member/login/sublogin.do")
-		 &&!uri.equals(path+"/member/register.do")
-		 &&!uri.equals(path+"/member/idcheck.do")
-		 &&!uri.equals(path+"/member/nickcheck.do")
-		 &&!uri.equals(path+"/member/login/subregister.do")) {
-			session.setAttribute("nextpage", uri);
-		}
+
+		session.setAttribute("nextpage", uri);
+
 		chain.doFilter(request, response);
 	}
 }
