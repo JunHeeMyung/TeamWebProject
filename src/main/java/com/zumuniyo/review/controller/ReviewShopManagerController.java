8a6750d150850ef9f8d1452e7fbe8cffd21202ac
Leedun <313lds@naver.com>
2022-06-14 15:24:07 +0900

    이덕수) ajax 관련 test

--- src/main/java/com/zumuniyo/review/controller/ReviewShopManagerController.java
+++ src/main/java/com/zumuniyo/review/controller/ReviewShopManagerController.java
@@ -48,32 +48,34 @@ public class ReviewShopManagerController implements Command {
 		List<ReviewShopDTO> reviewShopDTOs = service.selectByShopSeq_Manager(shop_seq);
 		
 		System.out.println("shop_seq :"+shop_seq);
+		
 		JSONArray jsonArray = new JSONArray();
 		
 	
 		for(ReviewShopDTO reviewShopDTO:reviewShopDTOs) {
 			
-			JSONObject jsonObject = new JSONObject();			
-			jsonObject.put("mem_seq", reviewShopDTO.getMem_seq());
-			jsonObject.put("menu_seq", reviewShopDTO.getMenu_seq());
-			jsonObject.put("review_taste", reviewShopDTO.getReview_taste());
-			jsonObject.put("review_amount", reviewShopDTO.getReview_amount());
-			jsonObject.put("review_service", reviewShopDTO.getReview_service());
-			jsonObject.put("review_content", reviewShopDTO.getReview_content());
-			jsonObject.put("review_content", reviewShopDTO.getReview_content());
+			JSONObject jsonObject = new JSONObject();		
 			
-			jsonObject.put("review_img", reviewShopDTO.getReview_img());
-			jsonObject.put("review_date", reviewShopDTO.getReview_date());
-			jsonObject.put("review_exposure", reviewShopDTO.getReview_exposure());
-			jsonObject.put("shop_seq", reviewShopDTO.getShop_seq());
-			jsonObject.put("menu_category", reviewShopDTO.getMenu_category());
-			jsonObject.put("menu_name", reviewShopDTO.getMenu_name());
-			jsonObject.put("menu_price", reviewShopDTO.getMenu_price());
-			jsonObject.put("menu_img", reviewShopDTO.getMenu_img());
-			jsonObject.put("menu_top", reviewShopDTO.getMenu_top());
-			jsonObject.put("menu_info", reviewShopDTO.getMenu_info());
-			jsonObject.put("menu_status", reviewShopDTO.getMenu_status());			
+			
+			jsonObject.put("mem_seq", reviewShopDTO.getMem_seq()+"");
+			jsonObject.put("menu_seq", reviewShopDTO.getMenu_seq()+"");
+			jsonObject.put("shop_seq", reviewShopDTO.getShop_seq()+"");
+			jsonObject.put("review_taste", reviewShopDTO.getReview_taste()+"");
+			jsonObject.put("review_amount", reviewShopDTO.getReview_amount()+"");
+			jsonObject.put("review_service", reviewShopDTO.getReview_service()+"");
+			jsonObject.put("menu_img", reviewShopDTO.getMenu_img()+"");
+			jsonObject.put("review_img", reviewShopDTO.getReview_img()+"");
+			jsonObject.put("review_date", reviewShopDTO.getReview_date()+"");		
+			jsonObject.put("review_content", reviewShopDTO.getReview_content()+"");				
+			jsonObject.put("review_exposure", reviewShopDTO.getReview_exposure()+"");
+			jsonObject.put("menu_category", reviewShopDTO.getMenu_category()+"");
+			jsonObject.put("menu_name", reviewShopDTO.getMenu_name()+"");
+			jsonObject.put("menu_price", reviewShopDTO.getMenu_price()+"");
+			jsonObject.put("menu_top", reviewShopDTO.getMenu_top()+"");
+			jsonObject.put("menu_info", reviewShopDTO.getMenu_info()+"");
+			jsonObject.put("menu_status", reviewShopDTO.getMenu_status()+"");			
 			jsonArray.add(jsonObject);
+		
 		}
 		
 		return "json:"+jsonArray;
