e9d0d9d29a3ace321c70feaee322db40148688fb
JunHee Myung <platinyan@naver.com>
2022-06-15 20:25:30 +0900

    에러메시지를 포함한 에러페이지

--- src/main/java/com/zumuniyo/common/ErrorServlet.java
+++ src/main/java/com/zumuniyo/common/ErrorServlet.java
@@ -12,7 +12,7 @@ public class ErrorServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/view/common/error404.jsp").forward(request, response);
+		request.getRequestDispatcher("/view/common/error.jsp").forward(request, response);
 	}
 
 }
