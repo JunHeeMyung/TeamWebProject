e0a6303f1569797b66a376958c504e23540f3275
JunHee Myung <platinyan@naver.com>
2022-06-15 20:33:10 +0900

    수정중

---
+++ src/main/java/com/zumuniyo/orderlist/model/OrderDetailController.java
@@ -0,0 +1,23 @@
+package com.zumuniyo.orderlist.model;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.zumuniyo.common.Command;
+
+public class OrderDetailController implements Command {
+
+	@Override
+	public String execute(HttpServletRequest request) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		return "json:디테일 준비중";
+	}
+
+}
---
+++ src/main/java/com/zumuniyo/orderlist/model/OrderlistServlet.java
@@ -0,0 +1,57 @@
+package com.zumuniyo.orderlist.model;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.zumuniyo.common.Command;
+
+@WebServlet("/orderlist/*")
+public class OrderlistServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String uri = request.getRequestURI().substring(request.getContextPath().length());
+		String method = request.getMethod();
+		/* System.out.println("요청:"+uri+":"+method); */
+		String page = "";
+		Command command = null;
+		
+		if(uri.equals("/orderlist/order.do")&&method.equals("POST")) {
+			command = new OrderController();
+		}else if(uri.equals("/orderlist/orderdetail.do")&&method.equals("GET")) {
+			command = new OrderDetailController();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		if(command==null) {
+			request.getRequestDispatcher("/error").forward(request, response);
+			return;
+		}
+		
+		page = command.execute(request);
+		
+		if(page.indexOf("json:")>=0) {
+			response.setContentType("text/html;charset=utf-8");
+			response.getWriter().print(page.substring(5));
+		}else if(page.indexOf("redirect:")>=0){
+			response.sendRedirect(page.substring(9));
+		}else {
+			request.getRequestDispatcher(page).forward(request, response);
+		}
+		
+	}
+
+}
