eba14a59ecc9b09febbcd2ae1b051a1038f96860
JunHee Myung <platinyan@naver.com>
2022-06-17 09:51:42 +0900

    zmny 회귀주소에 param 적용

--- src/main/java/com/zumuniyo/filter/PageFilter.java
+++ src/main/java/com/zumuniyo/filter/PageFilter.java
@@ -1,6 +1,8 @@
 package com.zumuniyo.filter;
 
 import java.io.IOException;
+import java.util.Map;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -21,11 +23,23 @@ public class PageFilter implements Filter {
 		// 로그인/로그아웃시 마지막 페이지로 이동하기위해 저장함
 		
 		String uri = ((HttpServletRequest)request).getRequestURI();
+		/* String queryString = request.gets */
+		
+		String params ="?";
+		boolean isFirstParam= true;
+		Map<String,String[]> paramMap = request.getParameterMap();
+		for(String key:paramMap.keySet()) {
+			if(isFirstParam) {
+				isFirstParam=false;
+			}else {
+				params+="&";
+			}
+			params+=(key+"="+paramMap.get(key)[0]);
+		}
 		
 		HttpSession session = ((HttpServletRequest)request).getSession();
-		
 
-		session.setAttribute("nextpage", uri);
+		session.setAttribute("nextpage", uri+params);
 
 		chain.doFilter(request, response);
 	}
